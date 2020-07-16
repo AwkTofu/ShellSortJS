abdd62bfa8115cb9ba6f5510eaccff379b063312
FanZhong Zeng <fanzhongzeng78@gmail.com>
2020-07-16 17:27:36 -0400

    added comments

--- shell_sort.js
+++ shell_sort.js
@@ -4,17 +4,18 @@ function shellSort(arr) {
 	let n = arr.length;
 	console.log("Array Length: ", n);
 	//Start with a really large gap, and then reduce the gap until there isn't any
+	//With this, the gap starts as half of the array length, and then half of that every time
 	for (let gap = Math.floor(n/2); gap > 0; gap = Math.floor(gap/2))
 	{
-		console.log("\nGap: ", gap);
-		console.log("Each Run: ",arr)
-		//Do a insertion sort for the gap
+
+		//Do a insertion sort for each of the section the gap ends up dividing
 		for (let i = gap; i < n; i += 1)
 		{
+			//We store the current varible
 			let temp = arr[i];
-			console.log("i", i)
-			console.log("Temp: ", temp);
-			let j;
+			
+			//This is the insection sort to sort the section into order
+			let j; //This is the insection sort to sort the section into order
 			for (j = i; j >= gap && arr[j-gap] > temp; j-=gap)
 			{
 				//console.log("test", arr[j-gap])
