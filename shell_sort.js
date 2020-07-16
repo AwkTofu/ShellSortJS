09c1fe215c371d90420b58b208afa3cd2a784e02
FanZhong Zeng <fanzhongzeng78@gmail.com>
2020-07-16 17:28:11 -0400

    Removed console.logs in the main function

--- shell_sort.js
+++ shell_sort.js
@@ -2,7 +2,6 @@ let array = [5, 7,2,3,4,1,6,58,-8,-1]
 
 function shellSort(arr) {
 	let n = arr.length;
-	console.log("Array Length: ", n);
 	//Start with a really large gap, and then reduce the gap until there isn't any
 	//With this, the gap starts as half of the array length, and then half of that every time
 	for (let gap = Math.floor(n/2); gap > 0; gap = Math.floor(gap/2))
@@ -18,10 +17,6 @@ function shellSort(arr) {
 			let j; //This is the insection sort to sort the section into order
 			for (j = i; j >= gap && arr[j-gap] > temp; j-=gap)
 			{
-				//console.log("test", arr[j-gap])
-				console.log("j",j,"j-gap",j-gap)
-				console.log("values j:",arr[j], "j-gap",arr[j-gap]);
-
 				arr[j] = arr[j-gap];
 			}
 
